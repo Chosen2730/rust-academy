433e65e38320c8a72694409921e402b37404c2bc
Chosen2730 <patiencesimoniseoluwa@gmail.com>
2024-09-29 13:39:49 +0100

    learnt how to use cargo

---
+++ 2. hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801.d
@@ -0,0 +1,5 @@
+/Users/mac/Development/Rust/rust-academy/2. hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801: src/main.rs
+
+/Users/mac/Development/Rust/rust-academy/2. hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801.d: src/main.rs
+
+src/main.rs:
---
+++ 2. hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mac/Development/Rust/rust-academy/2.\ hello_cargo/target/debug/hello_cargo: /Users/mac/Development/Rust/rust-academy/2.\ hello_cargo/src/main.rs
